94ded3b1ce88948fbd0e479ec8a8953d85d0bed8
Shubham Singh <ss889469@gmail.com>
2023-02-23 14:30:56 +0530

    navigation stack and screens added

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Home, AddNumbers } from "./src/containers";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="addNumbers" component={AddNumbers} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/containers/AddNumbers/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AddNumbers = () => {
+  return (
+    <View>
+      <Text>AddNumbers</Text>
+    </View>
+  );
+};
+
+export default AddNumbers;
